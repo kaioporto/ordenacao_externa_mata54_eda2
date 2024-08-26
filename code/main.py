91e4bea9987ff85838851163b7f03f51e53fa322
Kaio Porto <kaio.rodrigo28@gmail.com>
2024-08-25 23:25:31 -0300

    adicionando calculo beta_de_m_j e ajustando ordenacao multicaminhos

--- code/main.py
+++ code/main.py
@@ -1,24 +1,16 @@
 from sys import argv
-from struct import *
+import heapq
 
 def read_input_data():
-    filename = argv[1]          #argumento passado via CLI
-    with open(filename) as f:   #abrindo o arquivo e extraindo parametros
+    filename = argv[1]  # argumento passado via CLI
+    with open(filename) as f:  # abrindo o arquivo e extraindo parâmetros
         data = f.readlines()
     f.close()
 
     # organizando parâmetros a serem usados
-    alg = data[0]                          #char que indica o algoritmo
-    params = list(data[1].split(" "))      #parametros da funçao -> m, k, r, n
-    keys = list(data[2].split(" "))        #elementos a serem ordenados
-    
-    #convertendo pra inteiro
-    for i in range(len(params)):
-        params[i] = int(params[i])
-
-    #convertendo pra inteiro
-    for i in range(len(keys)):
-        keys[i] = int(keys[i])
+    alg = data[0].strip()  # char que indica o algoritmo (não utilizado diretamente aqui)
+    params = list(map(int, data[1].split()))  # parametros da função -> m, k, r, n
+    keys = list(map(int, data[2].split()))  # elementos a serem ordenados
 
     return params, keys
 
@@ -27,94 +19,96 @@ def heapify(elems, n, i):
     filho_esquerdo = 2*i+1
     filho_direito = 2*i+2
 
-    if(filho_esquerdo < n and elems[maior] < elems[filho_esquerdo]):
+    if filho_esquerdo < n and elems[maior] < elems[filho_esquerdo]:
         maior = filho_esquerdo
 
-    if(filho_direito < n and elems[maior] < elems[filho_direito]):
+    if filho_direito < n and elems[maior] < elems[filho_direito]:
         maior = filho_direito
 
-    if(maior != i):
-        elems[i], elems[maior] = elems[maior], elems[i] #troca
+    if maior != i:
+        elems[i], elems[maior] = elems[maior], elems[i]  # troca
 
-        #pra raiz
-        heapify(elems, n, maior) 
+        # heapifica a raiz
+        heapify(elems, n, maior)
 
 def heapsort(elems):
     n = len(elems)
 
-    for i in range(n//2 - 1, -1, -1): #do final pro início
+    for i in range(n//2 - 1, -1, -1):  # do final pro início
         heapify(elems, n, i)
-    
+
     for i in range(n-1, 0, -1):
         elems[i], elems[0] = elems[0], elems[i]
 
         heapify(elems, i, 0)
 
-def p_way_merge(mem_size, k_pages, runs, num_elements):
-    i = 1
-    j = 1
-    k = 0
-    while(i <= num_elements and j <= mem_size):
-        k = k+1
-        if(runs[i] < runs[j]):
-            runs[k] = runs[i]
-            i=i+1
-        else:
-            runs[k] = runs[j]
-            j=j+1
-    while(i < num_elements):
-        k=k+1
-        runs[k] = runs[i]
-        i=i+1
-    while(j < mem_size):
-        k=k+1
-        runs[k] = runs[j]
-        j=j+1
-
 def p_way_merge_driver(mem_size, k_pages, n_runs, keys):
-    max_tam_runs = len(keys)//mem_size #n/m
-    # if(alg == "B"):
-    #     k_pages = k_pages/2
+    max_tam_runs = len(keys) // mem_size  # n/m
     
     runs = []
     i = 0
     j = 0
+    num_writes = 0
     max_list = max_tam_runs
-    while j < n_runs:                   ## pega somente a r sequencias iniciais. o restante é descartado
-        inner_list = keys[i:max_list]   ##pegando os elementos da runs de tamanho maximo n/m
+    while j < n_runs:  # pega somente as r sequências iniciais. o restante é descartado
+        inner_list = keys[i:max_list]  # pegando os elementos das runs de tamanho máximo n/m
         runs.append(inner_list)
+        #calculo da B(m,j)
+        num_writes = num_writes + len(inner_list)
+
         i = i + max_tam_runs
         max_list = max_list + max_tam_runs
         j = j + 1
 
-    print(runs)
+    beta_de_m_j = (num_writes)/(n_runs * mem_size)
+    
+    print("fase 0 %.2f" % round(beta_de_m_j, 2), end=" ")
     for i in range(n_runs):
-        heapsort(runs[i])
+        heapsort(runs[i])  # Ordena cada run individualmente
 
     for i in range(n_runs):
-        #p_way_merge()
+        print(runs[i])
+    print("1:", runs)
+
+    # Intercalar as runs ordenadas
+    merged_output = p_way_merge(runs, k_pages)
+    #print("Resultado da intercalação:", merged_output)
+    return merged_output
+
+def p_way_merge(runs, k_pages):
+    heap = []
+    result = []
     
-    print(runs)
-    return runs
+    # Inicializa a heap com o primeiro elemento de cada run
+    for i in range(len(runs)):
+        if runs[i]:  # Verifica se a run não está vazia
+            heapq.heappush(heap, (runs[i][0], i, 0))  # (valor, índice da run, índice do elemento dentro da run)
+    
+
+    # Executa o processo de intercalação
+    while heap:
+        val, run_idx, elem_idx = heapq.heappop(heap)
+        result.append(val)
+        
+        # Adiciona o próximo elemento da mesma run à heap
+        if elem_idx + 1 < len(runs[run_idx]):
+            heapq.heappush(heap, (runs[run_idx][elem_idx + 1], run_idx, elem_idx + 1))
+
+    return result
 
 ########## DEBUG #############
-def show_data_debug(elems = None, var = None):
+def show_data_debug(elems=None, var=None):
     print("Elementos do array")
-    if(elems):
+    if elems:
         for elem in elems:
             print("%d" % int(elem), end=" ")
     
-    if(var):
-        print("Variavel")
+    if var:
+        print("Variável")
         print(var)
 
 
 params, keys = read_input_data()
 
-#show_data_debug(params)
-#show_data_debug(keys)
-#heapsort(keys)
-show_data_debug(keys)
+# Realiza o processo de ordenação e intercalação
 runs = p_way_merge_driver(params[0], params[1], params[2], keys)
-#show_data_debug(runs)
-#p_way_merge(params)
